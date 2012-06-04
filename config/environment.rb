c331c15f0abcbf0be9988b391f924ccd147e5faa
Tolulope Bukola <tbukola@gmail.com>
2012-06-04 17:44:53 -0500

    Initial Commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+ApApp1::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApApp1::Application.config.secret_token = '689336cd3b543969701f675b76406544b3ac6b03706514f4d77bdf2a1e0c6340e41ea0bc80de9e4598eadb034373af6bcc2e93de5a64fbd6a20a3ae420569d35'
