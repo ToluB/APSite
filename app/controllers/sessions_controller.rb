f5e41ed9934d9ed42e4d39c2954ee7c228fe894a
Tolulope Bukola <tbukola@gmail.com>
2012-06-26 00:06:48 -0500

    Get points for logging in every day

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,27 +1,54 @@
 class SessionsController < Devise::SessionsController
+  after_filter :user_login_merits, :only =>[:create]
   after_filter :update_user_merits, :only =>[:create]
+  #after_filter :update_user_rank, :only =>[:create]; update the user's ranking based on number of merits
+  
   def create
     redirect_to :back
   end
   
+  def user_login_merits
+    #Currently uses Western time zone; set to get 25 points for logging in on your birthday along with notice HAPPY BIRTHDAY!!
+    
+    if current_user.last_sign_in_at.in_time_zone(-5).day != Time.now.in_time_zone(-5).day
+      current_user.merits += 5
+    end
+  end
+  
   def update_user_merits
-    # This will calculate total merits for the current user, to be run on login
-
-    personal_merits = current_user.umerits #=> users earn this by logging in, creating posts and convos, and direct allocation from other users
-
-    #The next section will identify merits derived from the quality (as indicated by number of merits) of convos and posts
-
-    convo_merits = 0
-    post_merits = 0
-    current_user.convos.each{|convo| convo_merits+=convo.merits}
-    current_user.posts.each{|post| post_merits+=post.merits}
-    quality_merits = (convo_merits + post_merits)/5
-
-    current_user.merits = personal_merits + quality_merits
+    # This method is run on login; it calculates total merits for the user
+    # THIS IS THE HEART OF THE MERIT SYSTEM; ABSOLUTELY DO NOT F*** WITH THIS!
     
-    current_user.save
+    # Initializing two dummy variables:
     
-    #update ranks
+      convo_merits = 0
+      post_merits = 0   
+      
+    # personal merits = merits given directly to the user, minus merits given out by the user
+    # quality merits = the merits a user has derived from their posts and conversations
+    # umerits = quality merits as of a user's last login
+    
+      personal_merits = current_user.merits - current_user.umerits
+    
+    # calculating all the "quality merits" a user has ever earned from posting
+    
+      current_user.convos.each{|convo| convo_merits += convo.merits}
+      current_user.posts.each{|post| post_merits += post.merits}
+      quality_merits = (convo_merits + post_merits)/5
+    
+    #we'll save this as "umerits", so we can keep track for the next iteration
+      current_user.umerits = quality_merits
+      
+    # And finally, a user's total merits
+      
+      current_user.merits = personal_merits + quality_merits
+      current_user.save    
+  end
+  
+  def update_user_rank
+    if current_user.merits < 250
+      current_user.rank = "Tabula Rasa"
     
   end
+  
 end
