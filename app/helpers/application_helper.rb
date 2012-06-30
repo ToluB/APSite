2df1ab79cc9bf8c5ad61877a3b2a0226aadb48ae
Tolulope Bukola <tbukola@gmail.com>
2012-06-29 19:44:20 -0500

    Early implementation of tabs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,17 @@ module ApplicationHelper
     end
   end
   
+  def nav_link(link_text, link_path)
+     class_name = current_page?(link_path) ? 'active' : ''
+
+     content_tag(:li, :class => class_name) do
+       link_to link_text, link_path
+     end
+   end
+   
+   def no_forum_nav?
+     controller_name == 'invitations' || 
+     controller_name == 'kases' && action_name == 'index' 
+   end
+  
 end
