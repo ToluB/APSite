422f6364a1287d0fab3ff87ba68cd0a275eacd2e
Tolulope Bukola <tbukola@gmail.com>
2012-07-17 23:59:45 -0400

    Added admin attribute to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120709063623) do
+ActiveRecord::Schema.define(:version => 20120718035038) do
 
   create_table "collegeprefs", :force => true do |t|
     t.integer  "user_id"
@@ -167,6 +167,7 @@ ActiveRecord::Schema.define(:version => 20120709063623) do
     t.string   "location"
     t.string   "avatar"
     t.float    "gpa"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
