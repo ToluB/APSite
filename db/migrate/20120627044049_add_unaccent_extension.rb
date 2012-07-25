dfb082d99d7be6dd8403771b08f698629be3b8f3
Tolulope Bukola <tbukola@gmail.com>
2012-07-25 00:23:33 -0400

    Temporarily removing unaccent extension to gain function from pg search

--- db/migrate/20120627044049_add_unaccent_extension.rb
+++ db/migrate/20120627044049_add_unaccent_extension.rb
@@ -1,9 +1,9 @@
-class AddUnaccentExtension < ActiveRecord::Migration
-  def up
-    execute "create extension unaccent"
-  end
-
-  def down
-    execute "drop extension unaccent"
-  end
-end
+# class AddUnaccentExtension < ActiveRecord::Migration
+#   def up
+#     execute "create extension unaccent"
+#   end
+# 
+#   def down
+#     execute "drop extension unaccent"
+#   end
+# end
