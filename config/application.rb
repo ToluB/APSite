bb938a95ba3105083edf26f8846a6a7e8903a6bf
Tolulope Bukola <tbukola@gmail.com>
2012-07-24 23:07:36 -0400

    Heroku4..

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,8 @@ module ApApp1
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    #Do not load db till config vars are set...
+    #config.assets.initialize_on_precompile = false
   end
 end
